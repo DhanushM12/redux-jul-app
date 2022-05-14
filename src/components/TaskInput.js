2ef80d7e7d7d550e201fad5313adc4943d55526d
DhanushM12 <dhanushmukhi@gmail.com>
2022-05-14 12:14:29 +0530

    added reducer and action

---
+++ src/components/TaskInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TaskInput extends Component {
+    render() {
+        return (
+            <div>
+                taskinput
+            </div>
+        );
+    }
+}
+
+export default TaskInput;
